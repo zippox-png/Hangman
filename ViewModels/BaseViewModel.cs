60b24fa81ed6732399e557a2fcf995f37174fec5
zippox-png <stefania.dianaaz@gmail.com>
2026-04-15 15:12:21 +0300

    Add MVVM base infrastructure
    Implemented BaseViewModel (INotifyPropertyChanged) and RelayCommand for command binding

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Hangman.ViewModels
 {
-    class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? name = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(name);
+            return true;
+        }
     }
 }
